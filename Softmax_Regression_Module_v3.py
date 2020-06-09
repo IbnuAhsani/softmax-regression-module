c1aa6e7528d11d8bade8352249934c6eb3a60d41
IbnuAhsani <ibnu.muhari@gmail.com>
2020-06-09 10:47:18 +0700

    feat: update softmax regression module v.3

--- Softmax_Regression_Module_v3.py
+++ Softmax_Regression_Module_v3.py
@@ -12,15 +12,13 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-# %tensorflow_version 1.x
-
-# Commented out IPython magic to ensure Python compatibility.
 import os
 import sys
+import pickle
 import numpy as np
 import pandas as pd
 import seaborn as sn
-import tensorflow as tf
+import sklearn
 from sklearn import metrics 
 from imblearn.under_sampling import NearMiss
 from imblearn.over_sampling import SMOTE
@@ -37,9 +35,8 @@ np.set_printoptions(threshold=sys.maxsize)
 
 """**Data Preprocessing**"""
 
-data_frame = pd.read_csv('/content/drive/My Drive/Colab Notebooks/Content-Based Recommender System Data/Hypothesis Data/data-18-fv-tf-idf-scores-200.csv', header=None, sep=',')
+data_frame = pd.read_csv('/content/drive/My Drive/Colab Notebooks/Content-Based Recommender System Data/Hypothesis Data/data-23-tf-idf.csv', header=None, sep=',')
 data_frame.shape
-# /content/drive/My Drive/Colab Notebooks/Content-Based Recommender System Data/Hypothesis Data/data-10-fv-tf-idf-scores-7477-row-31-class-200-feature.csv
 
 header = []
 
@@ -64,7 +61,39 @@ total = sum(totals)
 for i in ax.patches:
     ax.text(i.get_x()-.03, i.get_height()+.5, i.get_height(), fontsize=10)
 
-data_frame_shuffled_once = data_frame.sample(frac=1)
+data_labels = data_frame['target']
+data_features = data_frame.drop('target',axis=1)
+
+near_miss = NearMiss(random_state=42)
+x_undersampled, y_undersampled = near_miss.fit_sample(data_features, data_labels)
+
+print(x_undersampled.shape)
+print(y_undersampled.shape)
+
+data_features_df = pd.DataFrame(data = x_undersampled[0:,0:], 
+                                index = [i for i in range(x_undersampled.shape[0])],
+                                columns = [str(i) for i in range(x_undersampled.shape[1])])
+
+data_labels_df = pd.DataFrame(data = y_undersampled[0:], 
+                                index = [i for i in range(y_undersampled.shape[0])],
+                                columns = ['target'])
+
+data_frame_undersampled = data_features_df.join(data_labels_df)
+
+ax = data_frame_undersampled['target'].value_counts().plot(kind='bar', figsize=(7,5), fontsize=12);
+ax.set_alpha(0.8)
+
+totals = []
+
+for i in ax.patches:
+    totals.append(i.get_height())
+
+total = sum(totals)
+
+for i in ax.patches:
+    ax.text(i.get_x()-.03, i.get_height()+.5, i.get_height(), fontsize=10)
+
+data_frame_shuffled_once = data_frame_undersampled.sample(frac=1)
 data_frame_shuffled_twice = data_frame_shuffled_once.sample(frac=1)
 data_frame_shuffled_twice.sample(3)
 
@@ -139,7 +168,6 @@ def get_fmeasure(labels, predictions):
 data_labels = data_frame_shuffled_twice['target'].to_numpy()
 data_features = data_frame_shuffled_twice.drop('target',axis=1).to_numpy()
 
-history = {}
 fold_counter = 1
 strat_kfold = StratifiedKFold(n_splits= num_k_splits)
 
@@ -178,6 +206,7 @@ for train_index, test_index in strat_kfold.split(data_features, data_labels):
 
   test_confusion_matrix = confusion_matrix(kfold_test_labels, test_prediction)
   confusion_matrix_df = pd.DataFrame(test_confusion_matrix, range(num_labels), range(num_labels))
+  
   plt.figure(figsize=(8,5))
   sn.set(font_scale=1.0)
   sn.heatmap(confusion_matrix_df, annot=True, fmt='g', cmap="BuPu", annot_kws={"size": 8}) 
@@ -185,15 +214,13 @@ for train_index, test_index in strat_kfold.split(data_features, data_labels):
   print('confusion matrix')
   plt.show()
 
+  # model_name = 'model_sastrawi_{}.pkl'.format(fold_counter)
+
+  # pickle.dump(model, open(model_name,'wb'))
+
   fold_counter += 1
 
 # near_miss = SMOTE()
-# x_undersampled, y_undersampled = near_miss.fit_sample(data_features, data_labels)
-
-# print(x_undersampled.shape)
-# print(y_undersampled.shape)
-
-# near_miss = NearMiss(random_state=42)
 # x_undersampled, y_undersampled = near_miss.fit_sample(data_features, data_labels)
 
 # print(x_undersampled.shape)
@@ -204,17 +231,3 @@ for train_index, test_index in strat_kfold.split(data_features, data_labels):
 
 # print(x_undersampled.shape)
 # print(y_undersampled.shape)
-
-# data_features_df = pd.DataFrame(data = x_undersampled[0:,0:], 
-#                                 index = [i for i in range(x_undersampled.shape[0])],
-#                                 columns = [str(i) for i in range(x_undersampled.shape[1])])
-
-# data_labels_df = pd.DataFrame(data = y_undersampled[0:], 
-#                                 index = [i for i in range(y_undersampled.shape[0])],
-#                                 columns = ['target'])
-
-# data_frame_undersampled = data_features_df.join(data_labels_df)
-
-# print(data_features_df.head())
-# print(data_labels_df.head())
-# print(data_frame_undersampled.head())
